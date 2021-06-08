174e026c5d773ec8ab9371385a35a5294366b2f7
SPie <d.spie@pm.me>
2021-06-08 20:35:06 +0200

    fixed table style for small screens

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="bg-black text-gray-100">
     <div class="min-h-screen ">
-      <h1 class="text-6xl font-black px-5 pt-5"><router-link to="/">FSKick</router-link></h1>
+      <h1 class="lg:text-6xl text-2xl font-black px-5 pt-5"><router-link to="/">FSKick</router-link></h1>
       <router-view/>
     </div>
     <footer class="text-center text-xs"><router-link to="/impressum">Impressum</router-link></footer>
--- src/views/seasons/Table.vue
+++ src/views/seasons/Table.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="py-10 px-10">
-    <div v-if="season" class="table-card xl:w-1/2 lg:w-3/4 md:w-3/4 sm:w-11/12 container hover:from-gray-800 hover:to-gray-200 hover:via-gray-500">
-      <h2 class="text-center text-2xl font-bold">Season {{season.name}}</h2>
-      <div class="px-10">
-        <table>
+  <div class="py-2 px-2">
+    <div v-if="season" class="mx-auto bg-gradient-to-b from-gray-900 to-gray-300 via-gray-600 p-5 rounded-lg shadow xl:w-1/2 lg:w-3/4 md:w-3/4 sm:w-11/12 container hover:from-gray-800 hover:to-gray-200 hover:via-gray-500">
+      <h2 class="text-center text-md md:text-2xl font-bold">Season {{season.name}}</h2>
+      <div class="">
+        <table class="mx-auto text-xs md:text-sm table-fixed">
           <thead>
             <tr>
-              <th>Position ({{playersCount}})</th>
-              <th>Player</th>
-              <th>Points Ratio</th>
-              <th>Points</th>
-              <th>Wins</th>
-              <th>Games ({{season.games.length}})</th>
-              <th>Games Ratio</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Pos ({{playersCount}})</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Player</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Points</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Wins</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Games ({{season.games.length}})</th>
+              <th class="border-b-2 text-left px-1 md:px-4 py-4">Games Ratio</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="player in playerStats" :key="player.name">
-              <td>{{player.position}}</td>
-              <td>{{player.name}}</td>
-              <td>{{player.pointsRatio}}</td>
-              <td>{{player.points}}</td>
-              <td>{{player.wins}}</td>
-              <td>{{player.games}}</td>
-              <td>{{gamesRatio(player.games)}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{player.position}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{player.name}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{player.pointsRatio}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{player.wins}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{player.games}}</td>
+              <td class="border-b-2 text-left px-1 md:px-4 py-2">{{gamesRatio(player.games)}}</td>
             </tr>
           </tbody>
         </table>
@@ -71,18 +69,3 @@ export default class Table extends Vue {
   }
 }
 </script>
-
-<style>
-th {
-  @apply border-b-2 text-left px-4 py-4
-}
-
-td {
-  @apply border-b-2 text-left px-4 py-2
-}
-
-.table-card {
-  @apply mx-auto bg-gradient-to-b from-gray-900 to-gray-300 via-gray-600 p-10 rounded-lg shadow
-}
-
-</style>
