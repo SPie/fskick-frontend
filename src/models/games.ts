d193aa1e2fb2803161adb189d938c5f061f9277e
SPie <d.spie@pm.me>
2022-10-28 23:58:39 +0200

    remove vue files

--- src/models/games.ts
+++
@@ -1,9 +0,0 @@
-export interface Game {
-    uuid: string
-}
-export interface Season {
-    uuid: string
-    name: string
-    active: boolean
-    games: Game[]
-}
--- src/models/players.ts
+++
@@ -1,9 +0,0 @@
-export interface PlayerStats {
-    uuid: string
-    name: string,
-    position: number,
-    pointsRatio: number,
-    points: number,
-    wins: number,
-    games: number
-}
