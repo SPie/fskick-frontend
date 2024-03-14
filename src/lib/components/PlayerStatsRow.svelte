ebbf3b3fd1b368846ea98976a6c02936927250bc
SPie <d.spie@pm.me>
2024-03-14 09:30:56 +0100

    Removed % from points column and uses percent value for win ratio

--- src/lib/components/PlayerStatsRow.svelte
+++ src/lib/components/PlayerStatsRow.svelte
@@ -9,8 +9,8 @@
 <tr>
   <PlayerStatsCell>{player.position}</PlayerStatsCell>
   <PlayerStatsCell><a href={`/player/${player.uuid}`} class="underline">{player.name}</a></PlayerStatsCell>
-  <PlayerStatsCell>{player.pointsRatio.toFixed(2)} %</PlayerStatsCell>
+  <PlayerStatsCell>{player.pointsRatio.toFixed(2)}</PlayerStatsCell>
   <PlayerStatsCell>{player.wins}</PlayerStatsCell>
   <PlayerStatsCell>{player.games} ({gamesCount ? (player.games / gamesCount * 100).toFixed(2) : 0} %)</PlayerStatsCell>
-  <PlayerStatsCell>{(player.wins / player.games).toFixed(2)} %</PlayerStatsCell>
+  <PlayerStatsCell>{(player.wins / player.games * 100).toFixed(2)} %</PlayerStatsCell>
 </tr>
